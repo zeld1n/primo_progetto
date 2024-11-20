e1d974b1cb283e7dd35c5b22797a71612c01ee9f
Chiril Bogaiciuc <zeldin2105@gmail.com>
2024-11-20 11:01:55 +0100

    Var

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from prima_app.views import homepage
 from prima_app.views import welcome
-from prima_app.views import lista,chi_siamo
+from prima_app.views import lista,chi_siamo,variabili
 
 app_name="prima_app"
 urlpatterns=[
     path('',homepage,name='homepage'),
     path('welcome',welcome,name='welcome'),
     path('lista',lista,name='lista'),
-    path('chi_siamo',chi_siamo,name='chi_siamo')
+    path('chi_siamo',chi_siamo,name='chi_siamo'),
+    path('variabili',variabili,name='variabili')
 ]
 
 
--- prima_app/views.py
+++ prima_app/views.py
@@ -13,3 +13,11 @@ def lista(request):
 
 def chi_siamo(request):
     return render(request,"prima_app/chi_siamo.html")
+
+def variabili(request):
+    context={
+        'var1':'Prima variabile',
+        'var2':'Seconda variabile',
+        'var3':'Terza variabile',
+    }
+    return render(request,"variabili.html",context)
